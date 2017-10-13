8db406796c7156f132cd09f9472adc6f3a6fcb74
TsukasaSumizono <tsukasa@M-TsukasaSumizono.local>
2017-10-13 16:24:51 +0900

    add assets

---
+++ Assets/Scripts/ChickenMoveScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMoveScript : MonoBehaviour {
+
+	private Touch touch;
+
+	private bool go = false;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		touch = GetComponent<Touch> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		string swipe = touch.Swipe ();
+
+		if(swipe == "right")
+		{
+			go = true;
+			Invoke ("GoOff",1.0f);
+
+		}
+
+		Move ();
+
+	}
+
+	private void GoOff()
+	{
+		go = false;
+	}
+
+	private void Move ()
+	{
+		if (go == true)
+		{
+			transform.position += new Vector3 (0, 0, 5 * Time.deltaTime);
+		}
+
+
+
+	}
+
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	public Touch touch;
+
+	void Awake()
+	{
+		touch = GetComponent<Touch> ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		//touch = Resources.Load ("Touch") as Touch;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		string swipe = "null";
+		swipe = touch.Swipe ();
+		Debug.Log (swipe);
+	}
+}
